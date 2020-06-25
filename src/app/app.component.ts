bdcde150e28882783b60fea1ff6ec227e5493cc9
Chong Li <Chong.L@debitsuccess.com>
2020-06-26 10:10:04 +1200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Post } from './posts/post.model';
+import { PostService } from './posts/post.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'mean';
+  posts: Post[] = [];
+  postsSubscription: Subscription;
+  constructor(public postService: PostService) { }
+
+  addPost(post){
+    this.postService.addPost(post);
+  }
+
+
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.postsSubscription = this.postService.getPostUpdatedListener().subscribe((posts) => {
+      this.posts = posts;
+    })
+  }
+
+  ngOnDestroy(){
+    this.postsSubscription.unsubscribe();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsModule } from './posts/posts.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PostsModule,
+    SharedModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
